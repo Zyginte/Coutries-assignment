60e265ee57f11750097bea396b2a348fc46304ca
Zyginte <zyginte.m@gmail.com>
2023-12-14 19:43:27 +0200

    Pabaigiau countries projekta

--- main.py
+++ main.py
@@ -3,21 +3,27 @@ class Country(object):
         self.population = population
         self.area = area
         self.name = name
+        self.population_density = self.population / self.area
 
     def is_big(self):
         if self.population > 20000000 or self.area > 3000000:
             return True
+        else:
+            return False
 
     def compare(self, another):
-        if self.population / self.area > another:
-            return f'{country} has a {smaller / larger} population density than {other_country}'
-
-    another = andorra
+        if self.population_density > another.population_density:
+            return f'{self.name} has a larger population density'
+        elif self.population_density < another.population_density:
+            return f'{self.name} has a smaller population density'
+        else:
+            return f'Population density is the same'
 
 
 australia = Country("Australia", 23545500, 7692024)
 andorra = Country("Andorra", 76098, 468)
 
-
-print(Country.is_big(self=australia))
-print(Country.compare(self=australia))
+print(andorra.is_big())
+print(australia.is_big())
+print(australia.compare(andorra))
+print(andorra.compare(australia))
